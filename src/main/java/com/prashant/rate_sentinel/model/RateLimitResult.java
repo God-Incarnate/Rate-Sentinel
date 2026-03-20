a42e26d9868bdd4627116e7da9bc9d5e917e5fa3
God-Incarnate <prashant11111999@gmail.com>
2026-03-21 01:32:47 +0530

    updates

--- src/main/java/com/prashant/rate_sentinel/model/RateLimitResult.java
+++ src/main/java/com/prashant/rate_sentinel/model/RateLimitResult.java
@@ -1,10 +1,13 @@
 package com.prashant.rate_sentinel.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RateLimitResult {
     private boolean allowed;
     private long remaining;
