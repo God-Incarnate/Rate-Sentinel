aaed2c6b2a1e0f606dd635132dd6a9e4ce386d4f
God-Incarnate <prashant11111999@gmail.com>
2026-05-29 20:16:03 +0530

    rate limit updates

--- src/main/java/com/prashant/rate_sentinel/filter/RateLimitFilter.java
+++ src/main/java/com/prashant/rate_sentinel/filter/RateLimitFilter.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import io.micrometer.core.instrument.Counter;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Map;
